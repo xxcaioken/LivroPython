7ca5feedfab2368cbab3f57a58f6f4d2456f4535
xxcaioken <cwkormives@gmail.com>
2022-02-23 01:00:23 -0300

    Add files via upload

---
+++ primeiros exemplos/primeiroExemplo.py
@@ -0,0 +1,8 @@
+print("óla Usuário!")
+numero = input("informe um numero inteiro:")
+print("O número é: ",numero)
+
+numero = int(numero)  #converte string para float
+dobro = numero * 2
+
+print("Dobro de numero: ",dobro)
---
+++ primeiros exemplos/segundoExemplo.py
@@ -0,0 +1,9 @@
+saudacao = "ola usuario, informe um numero real: "
+print(saudacao)
+numero = input()
+
+numero = float(numero)
+dobro = numero * 2
+
+saida = (" o dobro do numero informado é:", dobro)
+print(saida)
